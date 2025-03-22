2acadacbb5bb5721414f893f5b933a29270adb9b
bintang1855 <bintang.a.p1855@gmail.com>
2025-03-22 21:14:52 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300058/Program.cs
+++ tpmodul6_103022300058/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using tpmodul6_103022300058;
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Bintang Anugrah Pratama");
+        video.IncreasePlayCount(18052005);
+        video.PrintVideoDetails();
+    }
+}
+
---
+++ tpmodul6_103022300058/SayaTubeVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tpmodul6_103022300058
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Judul: " + this.title);
+            Console.WriteLine("ID Video: " + this.id);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+}
