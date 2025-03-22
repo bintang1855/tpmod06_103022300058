a995dc4c475b09e9da0b9fa76dfa9b6a7dc5231f
bintang1855 <bintang.a.p1855@gmail.com>
2025-03-22 22:02:21 +0700

    implementasi design by contract

--- tpmodul6_103022300058/Program.cs
+++ tpmodul6_103022300058/Program.cs
@@ -1,13 +1,23 @@
 ﻿using tpmodul6_103022300058;
 
 
-class Program 
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Bintang Anugrah Pratama");
-        video.IncreasePlayCount(18052005);
+        video.IncreasePlayCount(10000000);
         video.PrintVideoDetails();
+
+        for (int i = 0; i < 215; i++)
+        {
+            video.IncreasePlayCount(10000000); 
+        }
+        video.PrintVideoDetails();
+
+        Console.WriteLine();
+        Console.WriteLine("test prekonsidi");
+        video.IncreasePlayCount(100000000);
     }
 }
 
--- tpmodul6_103022300058/SayaTubeVideo.cs
+++ tpmodul6_103022300058/SayaTubeVideo.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using System.Diagnostics;
 
 namespace tpmodul6_103022300058
 {
@@ -10,15 +12,32 @@ namespace tpmodul6_103022300058
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(title != null, "Judul tidak boleh null.");
+            Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter.");
+
             Random rand = new Random();
             this.id = rand.Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
+
+            Contract.Invariant(this.playCount >= 0, "Invariant Error: playCount tidak boleh negatif.");
+            Debug.Assert(this.playCount >= 0, "Debug.Assert: playCount tidak boleh negatif.");
         }
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count >= 0 && count <= 10000000, "Penambahan play count tidak boleh negatif dan tidak melebihi 10.000.000");
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Play count melebihi batas maksimum integer.");
+            }
         }
 
         public void PrintVideoDetails()
